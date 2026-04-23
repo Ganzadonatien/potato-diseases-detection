09712d5bc72ead74be32c24291e2a863ab49f962
Ganzadonatien <nzizadonatien@gmail.com>
2026-04-23 14:25:04 +0200

    ai powered

--- irish_potato_app/lib/screens/forgot_password.dart
+++ irish_potato_app/lib/screens/forgot_password.dart
@@ -1,54 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:irish_potato_app/widgets/custom_scaffold.dart';
-
-class ForgotPasswordScreen extends StatefulWidget {
-  const ForgotPasswordScreen({super.key});
-
-  @override
-  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
-}
-
-class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return CustomScaffold(
-      child: Column(
-        children: [
-          Flexible(
-            flex: 3,
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-              child: Center(
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: const TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'FORGOT PASSWORD\n',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 45,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1.2,
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Enter your email to reset your password',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 1.2,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+  class ForgotPasswordScreen extends StatefulWidget {
+    const ForgotPasswordScreen({super.key});
+  
+    @override
+    State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
   }
-}
+  
+  class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+    @override
+    Widget build(BuildContext context) {
+      return const Text('Forgot Password');
+    }
+  }
